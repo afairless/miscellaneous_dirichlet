3058e7f66d285be2cc0d1276fe6648e9bfd4b15c
gv <afairless@gmail.com>
2024-05-25 06:25:52 -0400

    turn debug off for deployment

--- dirichlet_dash_app01/src/dirichlet_multinomial.py
+++ dirichlet_dash_app01/src/dirichlet_multinomial.py
@@ -352,4 +352,4 @@ def plot_beta_03(n_intervals: int):
 
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050, debug=True)
+    app.run_server(host='0.0.0.0', port=8050, debug=False)
--- dirichlet_dash_app02/src/dirichlet_multinomial.py
+++ dirichlet_dash_app02/src/dirichlet_multinomial.py
@@ -962,4 +962,4 @@ def plot_beta_03(
 
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port=8050, debug=True)
+    app.run_server(host='0.0.0.0', port=8050, debug=False)
